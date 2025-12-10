e92072eb939ca7e375473aca0b4990772963fd91
TilmanNiem <tilman.niem@gmail.com>
2025-12-10 23:16:04 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login').then(m => m.Login),
+        children: [
+            {
+                path: 'register',
+                loadComponent: () => import('./login/register/register').then(m => m.Register)
+            }
+        ],
+
+    }
+];
