4cf54bfef9024dc3ee534b4e946b1e55a3d22cd3
TilmanNiem <tilman.niem@gmail.com>
2025-12-11 19:28:48 +0100

    fix routing and start implementing signal forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import('./login/login').then(m => m.Login),
-        children: [
-            {
-                path: 'register',
-                loadComponent: () => import('./login/register/register').then(m => m.Register)
-            }
-        ],
-
-    }
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register').then((m) => m.Register),
+  },
 ];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { InputText } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonModule, DividerModule, InputText, PanelModule],
+  imports: [ButtonModule, DividerModule, InputText, PanelModule, FloatLabelModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
