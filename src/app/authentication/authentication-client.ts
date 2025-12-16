8639574f80f86781209487f8ea7d76c001a7032e
TilmanNiem <tilman.niem@gmail.com>
2025-12-16 21:24:19 +0100

    update urls

--- src/app/authentication/authentication-client.ts
+++ src/app/authentication/authentication-client.ts
@@ -11,13 +11,13 @@ import { email } from "@angular/forms/signals";
 export class AuthenticationClient {
     private readonly httpClient = inject(HttpClient);
 
-    private readonly BASE_API_URL = 'http://127.0.0.1:8000/users/';
+    private readonly BASE_API_URL = 'http://127.0.0.1:8000/users';
 
     registerUser(userData: Registration): Observable<Registration> {
         return this.httpClient.post<Registration>(this.BASE_API_URL, userData);
     }
 
     loginUser(cred: Credentials): Observable<Credentials> {
-        return this.httpClient.post<Credentials>(this.BASE_API_URL + 'login/', cred);
+        return this.httpClient.post<Credentials>(this.BASE_API_URL + '/login', cred);
     }
 }
